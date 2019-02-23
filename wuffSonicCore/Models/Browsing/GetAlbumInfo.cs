aa89ff6ceeef9d6c030b5b722719d59573e7d67a
bitrvmpd <e.noyer.silva@gmail.com>
2019-02-23 12:14:30 -0600

    :sparkles: Adding GetAlbumInfo and GetAlbumInfo2

---
+++ wuffSonicCore/Models/Browsing/GetAlbumInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace wuffSonic.Models
+{
+    [XmlRoot(ElementName = "subsonic-response", Namespace = "http://subsonic.org/restapi")]
+    public class GetAlbumInfoResponse
+    {
+        [XmlAttribute(AttributeName = "version")]
+        public string version { get; set; }
+        [XmlAttribute(AttributeName = "status")]
+        public string status { get; set; }
+        [XmlElement(ElementName ="albumInfo")]
+        public AlbumInfo albumInfo { get; set; }
+    }
+
+    public class AlbumInfo{
+        [XmlElement(ElementName ="notes")]
+        public string notes{get;set;}
+        [XmlElement(ElementName ="musicBrainzId")]
+        public string musicBrainzId{get;set;}
+        [XmlElement(ElementName ="lastFmUrl")]
+        public string lastFmUrl{get;set;}
+        [XmlElement(ElementName ="smallImageUrl")]
+        public string smallImageUrl{get;set;}
+        [XmlElement(ElementName ="mediumImageUrl")]
+        public string mediumImageUrl{get;set;}
+        [XmlElement(ElementName ="largeImageUrl")]
+        public string largeImageUrl{get;set;}        
+    }
+    public class GetAlbumInfo : Request<GetAlbumInfoResponse>
+    {
+        /// <summary>
+        /// Returns album notes, image URLs etc, using data from last.fm
+        /// </summary>
+        /// <param name="id">The album or song ID.</param>
+        public GetAlbumInfo(string id)
+           : base(nameof(id), id)
+        {
+
+        }
+        public GetAlbumInfoResponse Response
+        {
+            get
+            {
+                return (GetAlbumInfoResponse)_response;
+            }
+        }
+        public override string method
+        {
+            get
+            {
+                return "getAlbumInfo";
+            }
+        }
+    }
+}
+
---
+++ wuffSonicCore/Models/Browsing/GetAlbumInfo2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace wuffSonic.Models
+{    
+    public class GetAlbumInfo2 : Request<GetAlbumInfoResponse>
+    {
+        /// <summary>
+        /// Similar to getAlbumInfo2, but organizes music according to ID3 tags.
+        /// </summary>
+        /// <param name="id">The album ID.</param>
+        public GetAlbumInfo2(string id)
+           : base(nameof(id), id)
+        {
+
+        }
+        public GetAlbumInfoResponse Response
+        {
+            get
+            {
+                return (GetAlbumInfoResponse)_response;
+            }
+        }
+        public override string method
+        {
+            get
+            {
+                return "getAlbumInfo2";
+            }
+        }
+    }
+}
+
